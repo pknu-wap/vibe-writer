736bf6e2c9d21fc77ee738c24938b3f9baaee671
pwc0319 <lhy060502@gmail.com>
2026-05-29 00:29:13 +0900

    디자인 수정

--- FE/src/app/components/top-nav.js
+++ FE/src/app/components/top-nav.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+import "./top-nav.css";
+
 export default function TopNav() {
   return (
     <div className="top-bar">
-      <h1 className="logo">VIBE - WRITER</h1>
+      <Link href="/" className="logo-link">
+        <h1 className="logo">VIBE - WRITER</h1>
+      </Link>
 
       <div className="line"></div>
 
