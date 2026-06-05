61a51335faabee7f42d7b06b714d6270a470a1ba
komm-tod <jupiter7422@naver.com>
2026-06-05 17:39:49 +0900

    Update process.py

--- BE/backend-api/process.py
+++ BE/backend-api/process.py
@@ -21,8 +21,8 @@ FONT_SIZE_SCALE = 4
 POSITION_MARGIN_V = {
     "상단": 120,    # 위에서 약간 떨어진 곳
     "중앙": 0,      # 정중앙
-    "하단": 500,    # 아래에서 좀 떨어진 곳
-    "top": 120, "middle": 0, "bottom": 500,
+    "하단": 450,    # 아래에서 좀 떨어진 곳
+    "top": 120, "middle": 0, "bottom": 450,
 }
 
 POSITION_ALIGNMENT = {
